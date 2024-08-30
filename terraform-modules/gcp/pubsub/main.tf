f51ab21c757bdea8194394b4ac114d0d6c33e89d
Will <651833+WillBeebe@users.noreply.github.com>
2024-08-30 00:18:30 -0700

    solve api enable later

--- terraform-modules/gcp/pubsub/main.tf
+++ terraform-modules/gcp/pubsub/main.tf
@@ -8,14 +8,16 @@ resource "google_pubsub_topic" "main" {
   message_retention_duration = "86600s"
 }
 
-locals {
-  apis_to_enable = ["pubsub.googleapis.com"]
-}
+# todo: manage this elsewhere, like a pre-provision step. 
+# it's awk to ship it here IMO
+# locals {
+#   apis_to_enable = ["pubsub.googleapis.com"]
+# }
 
-resource "google_project_service" "enable_apis" {
-  for_each = toset(local.apis_to_enable)
-  project  = var.gcp_authentication.project_id
-  service  = each.value
+# resource "google_project_service" "enable_apis" {
+#   for_each = toset(local.apis_to_enable)
+#   project  = var.gcp_authentication.project_id
+#   service  = each.value
 
-  disable_on_destroy = false
-}
+#   disable_on_destroy = false
+# }
